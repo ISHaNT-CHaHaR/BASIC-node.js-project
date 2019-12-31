e31a81ed70a389bff67fa34910d5e281a45e460d
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2019-12-31 14:17:32 +0530

    updated index.js
    code ready for mapping in JSON file

--- index.js
+++ index.js
@@ -25,7 +25,21 @@ console.log("will read file!");
 ///////////////////////////////////////////////////////////////////////////////////////////
 
 //SERVER
- 
+ const replaceTemplate=(temp,product)=>{
+     let output = temp.replace(/{%PRODUCTNAME%}/g,product.productName);
+     output=output.replace(/{%IMAGE%}/g,product.image); 
+     output=output.replace(/{%PRICE%}/g,product.price); 
+     output=output.replace(/{%FROM%}/g,product.from); 
+     output=output.replace(/{%NUTRIENTS%}/g,product.nutrients); 
+     output=output.replace(/{%QUANTITY%}/g,product.quantity); 
+     output=output.replace(/{%DESCRIPTION%}/g,product.description); 
+     output=output.replace(/{%ID%}/g,product.id); 
+        
+     if(!product.organic){
+        output=output.replace(/{%NOT_ORGANIC%}/g, 'not-organic'); 
+     }
+
+ };
 const data =  fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8'); 
 const tempOverview =  fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8'); 
 const tempCard =  fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8'); 
@@ -42,6 +56,9 @@ const server=http.createServer((req,res)=>{
     {
 
         res.writeHead(200,{'Content-type':'text/html'});
+         const cardsHtml=dataObj.map(el=>replaceTemplate(tempCard, el ){
+
+         })   
         res.end(tempOverview);
 
     } 
